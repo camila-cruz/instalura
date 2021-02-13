cf950e081c15bab5bd3fd6f1c3bf2529b1cd6b24
Camila <cruz.camila001@gmail.com>
2021-02-13 14:22:20 -0300

    Adiciona o logo do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Menu from '../src/components/commons/Menu';
 
 export default function Home() {
-  return <Title>Instalura</Title>
+    return (
+        <div>
+            <Menu />
+        </div>
+    )
 }
