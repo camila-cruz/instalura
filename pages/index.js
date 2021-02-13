80c245474e1450051a0cae371325af8099c83a60
Camila <cruz.camila001@gmail.com>
2021-02-13 18:39:43 -0300

    Cria o componente de footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Menu from '../src/components/commons/Menu';
+import Footer from '../src/components/commons/Footer';
 
 export default function Home() {
     return (
-        <div>
+        <div style={{
+            display: 'flex',    // Deixa o footer na parte de baixo da tela
+            flex: '1',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+        }}>
             <Menu />
+            <Footer />
         </div>
     )
 }
