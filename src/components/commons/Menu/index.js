e40700f41e269b8df1082ac4525c655fa2247ab4
Camila <cruz.camila001@gmail.com>
2021-02-13 14:22:01 -0300

    Adiciona o menu no topo da página

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Logo from '../../../theme/Logo';
+
+import { MenuWrapper, LeftSide, CentralSide, RightSide } from './styles/MenuWrapper';
+
+const links = [
+    {
+        texto: 'Home',
+        url: '/'
+    },
+    {
+        texto: 'Perguntas frequentes',
+        url: '/faq'
+    },
+    {
+        texto: 'Sobre',
+        url: '/sobre'
+    }
+]
+
+export default function Menu() {
+
+    return (
+        <MenuWrapper>
+            <MenuWrapper.LeftSide>
+                <Logo />
+            </MenuWrapper.LeftSide>
+            <MenuWrapper.CentralSide>
+                {links.map((link) => {
+                    return (
+                        <li>
+                            <a href={link.url}>
+                                {link.texto}
+                            </a>
+                        </li>
+                    )
+                })}
+            </MenuWrapper.CentralSide>
+            <MenuWrapper.RightSide>
+                <button>
+                    Entrar
+                </button>
+                <button>
+                    Cadastrar
+                </button>
+            </MenuWrapper.RightSide>
+        </MenuWrapper>
+    )
+}
