17553e0b4d0e852cac0dcea6533ffe638f7aa1f4
Camila <cruz.camila001@gmail.com>
2021-02-27 11:35:24 -0300

    Cria formulário de cadastro na Modal

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -122,6 +122,7 @@ const Col = styled.div`
   ${propToStyle('flexDirection')}
   ${propToStyle('alignItems')}
   ${propToStyle('justifyContent')}
+  ${propToStyle('paddingRight')}
 `;
 
 Col.defaultProps = {
@@ -134,6 +135,11 @@ const Row = styled.div`
   flex-wrap: wrap;
   margin-right: -16px;
   margin-left: -16px;
+
+  ${propToStyle('flex')}
+  ${propToStyle('marginLeft')}
+  ${propToStyle('marginRight')}
+  ${propToStyle('justifyContent')}
 `;
 
 // eslint-disable-next-line import/prefer-default-export
--- src/theme/utils/propToStyle.js
+++ src/theme/utils/propToStyle.js
@@ -1,11 +1,10 @@
 /* eslint-disable consistent-return */
 import { breakpointsMedia } from './breakpointsMedia';
 
-// eslint-disable-next-line import/prefer-default-export
 export function propToStyle(propName) {
   // closure
   // eslint-disable-next-line func-names
-  return function (props) {
+  return function (props) { // Props do componente que chama essa função
     const propValue = props[propName]; // String ou objeto
 
     if (typeof propValue === 'string' || typeof propValue === 'number') {
