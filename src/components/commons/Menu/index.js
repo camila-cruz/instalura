87004fe385ddcbc2ecf9b3e20d4cb8193a85a935
Camila <cruz.camila001@gmail.com>
2021-03-10 21:42:45 -0300

    feat(pages): add login page

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -36,7 +36,7 @@ export default function Menu({ onCadastrarClick }) {
         ))}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">
+        <Button ghost variant="secondary.main" href="/app/login">
           Entrar
         </Button>
         <Button variant="primary.main" onClick={onCadastrarClick}>
