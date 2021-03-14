37574e8b558b4868c014b086bf5138dcf83d63b7
Camila <cruz.camila001@gmail.com>
2021-03-14 19:33:22 -0300

    feat: cria hoc para renderizar as páginas

---
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import WebsitePageWrapper from '..';
+import WebsiteGlobalProvider from '../provider';
+
+export default function websitePageHOC(PageComponent, { pageWrapperProps }) {
+  /* Props injetadas pelo NextJS (ex.: retorno da getStaticProps) */
+  return (props) => (
+    <WebsiteGlobalProvider>
+      <WebsitePageWrapper {...pageWrapperProps}>
+        <PageComponent {...props} />
+      </WebsitePageWrapper>
+    </WebsiteGlobalProvider>
+  );
+}
