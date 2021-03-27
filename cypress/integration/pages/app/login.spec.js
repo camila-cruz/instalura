5b8ef210a583f439677730a7300282ed31e29741
Camila <cruz.camila001@gmail.com>
2021-03-27 16:53:15 -0300

    fix: configurações do linter com cypress

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-//import { describe } from "mocha";
+// import { describe } from "mocha";
 
 describe('/pages/app/login', () => {
   // it é o teste que estamos fazendo
@@ -10,13 +10,13 @@ describe('/pages/app/login', () => {
 
     // Preencher o input de usuário
     cy.get('#formCadastro input[name="usuario"]').type('camilacruz');
-    
+
     // Preencher o input de senha
     cy.get('#formCadastro input[name="senha"]').type('senhasegura');
-    
+
     // Clicar no botão de submit
     cy.get('#formCadastro button[type="submit"]').click();
-    
+
     // O que esperamos? Ir para /app/profile
     cy.url().should('include', '/app/profile');
   });
