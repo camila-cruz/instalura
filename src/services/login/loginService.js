e4cc230bf0f071c6d9e3fee83b0ab704b2519687
Camila <cruz.camila001@gmail.com>
2021-03-27 18:54:29 -0300

    feat: adiciona o service de Login

---
+++ src/services/login/loginService.js
@@ -0,0 +1,30 @@
+async function HttpClient(url, { headers, body, ...options }) {
+  return fetch(url, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    ...options,
+  })
+    .then((respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        return respostaDoServidor.json();
+      }
+
+      throw new Error('Falha em pegar os dados do servidor :(');
+    })
+    .then((respostaConvertida) => respostaConvertida);
+}
+
+export const loginService = {
+  async login({ username, password }) {
+    return HttpClient('https://instalura-api-git-master-omariosouto.vercel.app/api/login', {
+      method: 'POST',
+      body: {
+        username,
+        password,
+      },
+    });
+  },
+};
