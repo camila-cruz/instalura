bafa2c58a1451d40aa89266033d83397ae968cbf
Camila <cruz.camila001@gmail.com>
2021-03-27 22:16:15 -0300

    test: cookie com token de login

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -5,6 +5,9 @@
 describe('/pages/app/login', () => {
   // it é o teste que estamos fazendo
   it('preencha os campos e vá para a página /app/profile', () => {
+    cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
+      .as('userLogin');
+
     // Vai até esta página
     cy.visit('/app/login');
 
@@ -19,5 +22,15 @@ describe('/pages/app/login', () => {
 
     // O que esperamos? Ir para /app/profile
     cy.url().should('include', '/app/profile');
+
+    // Temos o token?
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        const { token } = intercept.response.body.data;
+
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token); // Token do cookie é igual ao token do server?
+      });
   });
 });
