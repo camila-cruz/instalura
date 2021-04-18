39c1a9420c2b11c9ab1eb04e1efeb820cfb75c00
Camila <cruz.camila001@gmail.com>
2021-04-18 19:04:44 -0300

    feat: exibe descrição correspondente ao post

--- src/components/screens/app/ProfileScreen/Post/PostComments/index.js
+++ src/components/screens/app/ProfileScreen/Post/PostComments/index.js
@@ -1,11 +1,19 @@
-import { get } from 'lodash';
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { Grid } from '../../../../../foundation/layout/Grid';
 
 const Comments = styled.div`
   display: flex;
   border: 1px solid green;
+  padding-bottom: 24px;
+`;
+
+const PostDescription = styled.div`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const CommentsButton = styled.button`
@@ -19,15 +27,15 @@ const CommentsButton = styled.button`
   color: ${(props) => get(props.theme, 'colors.tertiary.light.contrastText')}; */
 `;
 
-export function PostComments() {
+export function PostComments({ description }) {
   return (
     <Comments>
       <Grid.Col>
         avatares
       </Grid.Col>
-      <Grid.Col>
-        texto
-      </Grid.Col>
+      <PostDescription>
+        {description}
+      </PostDescription>
       <Grid.Col>
         <CommentsButton>
           Mais
@@ -36,3 +44,7 @@ export function PostComments() {
     </Comments>
   );
 }
+
+PostComments.propTypes = {
+  description: PropTypes.string.isRequired,
+};
