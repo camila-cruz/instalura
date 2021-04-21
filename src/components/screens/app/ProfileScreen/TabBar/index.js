7675ce1f250406a77b5082fb2d6ea444237e2019
Camila <cruz.camila001@gmail.com>
2021-04-20 21:00:47 -0300

    feat: adiciona ícones ao conteúdo da TabBar

--- src/components/screens/app/ProfileScreen/TabBar/index.js
+++ src/components/screens/app/ProfileScreen/TabBar/index.js
@@ -1,49 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Grid } from '../../../../foundation/layout/Grid';
+import {
+  HomeOutline as HomeIcon,
+  SearchOutline as SearchIcon,
+  PlusOutline as AddIcon,
+  HeartOutline as HeartIcon,
+} from '@styled-icons/evaicons-outline';
+import { Avatar } from '../../../../commons/Avatar';
 
 const TabBarWrapper = styled.div`
-  height: 64px;
-  width: 100%;
   position: fixed;
   bottom: 0;
+  left: 0;
+  /* height: 64px; */
+  width: 100%;
+  width: -webkit-fill-available;
+
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 26px;
+
   border-top-left-radius: 24px;
   border-top-right-radius: 24px;
+
   background-color: yellow;
   z-index: 99;
+`;
+
+const Tab = styled.div`
+  height: auto;
 `;
 
 export function TabBar() {
   return (
     <TabBarWrapper>
-      <Grid.Row>
-        <Grid.Col
-          offset={1}
-          value={2}
-        >
-          home
-        </Grid.Col>
-        <Grid.Col
-          value={2}
-        >
-          search
-        </Grid.Col>
-        <Grid.Col
-          value={2}
-        >
-          add
-        </Grid.Col>
-        <Grid.Col
-          value={2}
-        >
-          like
-        </Grid.Col>
-        <Grid.Col
-          value={2}
-        >
-          profile
-        </Grid.Col>
-      </Grid.Row>
+      <Tab>
+        <HomeIcon size={24} />
+      </Tab>
+      <Tab>
+        <SearchIcon size={24} />
+      </Tab>
+      <Tab>
+        <AddIcon size={24} />
+      </Tab>
+      <Tab>
+        <HeartIcon size={24} />
+      </Tab>
+      <Tab>
+        <Avatar src="https://via.placeholder.com/32" size={24} />
+      </Tab>
     </TabBarWrapper>
   );
 }
