65f0021c4a074dd744bb67f83ed8097956c9881b
Camila <cruz.camila001@gmail.com>
2021-04-21 12:16:07 -0300

    refactor: adiciona dark theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,61 +1,124 @@
 import { typographyVariants } from './typographyVariants';
 
-export const colors = {
-  background: {
-    light: {
-      color: '#FFFFFF',
+export const lightTheme = {
+  colors: {
+    background: {
+      light: {
+        color: '#FFFFFF',
+      },
+      main: {
+        color: '#F2F2F2',
+      },
     },
-    main: {
-      color: '#F2F2F2',
+    borders: {
+      main: {
+        color: '#F1F1F1',
+      },
     },
-  },
-  borders: {
-    main: {
-      color: '#F1F1F1',
+    primary: {
+      main: {
+        color: '#D7385E',
+        contrastText: '#fff',
+      },
     },
-  },
-  primary: {
-    main: {
-      color: '#D7385E',
-      contrastText: '#fff',
+    secondary: {
+      main: {
+        color: '#FB7B6B',
+        contrastText: '#fff',
+      },
     },
-  },
-  secondary: {
-    main: {
-      color: '#FB7B6B',
-      contrastText: '#fff',
+    tertiary: {
+      main: {
+        color: '#070C0E',
+        contrastText: '#fff',
+      },
+      light: {
+        color: '#88989E',
+        contrastText: '#fff',
+      },
     },
-  },
-  tertiary: {
-    main: {
-      color: '#070C0E',
-      contrastText: '#fff',
+    // Feedback colors
+    error: {
+      main: {
+        color: '#dc3545',
+        contrastText: '#fff',
+      },
     },
-    light: {
-      color: '#88989E',
-      contrastText: '#fff',
+    success: {
+      main: {
+        color: '#28a745',
+        contrastText: '#fff',
+      },
     },
-  },
-  // Feedback colors
-  error: {
-    main: {
-      color: '#dc3545',
-      contrastText: '#fff',
+    warning: {
+      main: {
+        color: '#F77228',
+        constrastText: '',
+      },
     },
-  },
-  success: {
-    main: {
-      color: '#28a745',
-      contrastText: '#fff',
-    },
-  },
-  modes: {
-    dark: {},
   },
 };
 
-export default {
-  colors,
+export const darkTheme = {
+  colors: {
+    background: {
+      light: {
+        color: '#030506',
+      },
+      main: {
+        color: '#030506',
+      },
+    },
+    borders: {
+      main: {
+        color: '#181F22',
+      },
+    },
+    primary: {
+      main: {
+        color: '#F27895',
+        contrastText: '#fff',
+      },
+    },
+    secondary: {
+      main: {
+        color: '#FFA59A',
+        contrastText: '#fff',
+      },
+    },
+    tertiary: {
+      main: {
+        color: '#D4D4D4',
+        contrastText: '#fff',
+      },
+      light: {
+        color: '#FFFFFF',
+        contrastText: '#000',
+      },
+    },
+    // Feedback colors
+    error: {
+      main: {
+        color: '#EB5C50',
+        contrastText: '#fff',
+      },
+    },
+    success: {
+      main: {
+        color: '#28a745',
+        contrastText: '#fff',
+      },
+    },
+    warning: {
+      main: {
+        color: '#FB9E6B',
+        constrastText: '',
+      },
+    },
+  },
+};
+
+const defaultTheme = {
   typographyVariants,
   breakpoints: {
     xs: 0,
@@ -68,3 +131,6 @@ export default {
   borderRadius: '8px',
   transition: '200ms ease-in-out',
 };
+
+export const light = { ...lightTheme, ...defaultTheme };
+export const dark = { ...darkTheme, ...defaultTheme };
