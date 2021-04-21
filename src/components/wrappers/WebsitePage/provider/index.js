3545d702454de730c07ae9736c546ddc2ef96d7e
Camila <cruz.camila001@gmail.com>
2021-04-21 17:19:21 -0300

    fix: comparação de objetos do tema

--- src/components/wrappers/WebsitePage/provider/index.js
+++ src/components/wrappers/WebsitePage/provider/index.js
@@ -9,23 +9,26 @@ import { ThemeSwitcher } from '../../../commons/ThemeSwitcher';
 export default function WebsiteGlobalProvider({ children }) {
   const [theme, setTheme] = React.useState(() => {
     const { APP_THEME } = parseCookies(null);
-    return APP_THEME === 'light' ? light : dark;
+    if (APP_THEME) {
+      return APP_THEME === 'light' ? 'light' : 'dark';
+    }
+    return 'light';
   });
 
   useEffect(() => {
-    setCookie(null, 'APP_THEME', theme === light ? 'light' : 'dark', {
+    setCookie(null, 'APP_THEME', theme, {
       path: '/',
       maxAge: 30 * 24 * 60 * 60,
     });
   }, [theme]);
 
   function toggleTheme() {
-    setTheme(theme === light ? dark : light);
+    setTheme(theme === 'light' ? 'dark' : 'light');
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <ThemeSwitcher onClick={toggleTheme} theme={theme === light ? 'light' : 'dark'} />
+    <ThemeProvider theme={theme === 'light' ? light : dark}>
+      <ThemeSwitcher onClick={toggleTheme} theme={theme} />
       <GlobalStyle />
       {children}
     </ThemeProvider>
