54922c9e08034528afde0a4645b145ba11f96d8e
Camila <cruz.camila001@gmail.com>
2021-05-01 17:13:44 -0300

    refactor: página de perfil básica para outros usuários

--- pages/app/profile/[id].js
+++ pages/app/profile/[id].js
@@ -2,11 +2,12 @@
 import React from 'react';
 import UserScreen from '../../../src/components/screens/app/UserScreen';
 import websitePageHOC from '../../../src/components/wrappers/WebsitePage/hoc';
-import { userService } from '../../../src/services/user/userService';
-import { authService } from '../../../src/services/auth/authService';
+// import { userService } from '../../../src/services/user/userService';
+// import { authService } from '../../../src/services/auth/authService';
 
-function OtherUsersProfilePage({ userInfo, posts }) {
-  return <UserScreen userInfo={userInfo} posts={posts} />;
+// function OtherUsersProfilePage({ userInfo, posts }) {
+function OtherUsersProfilePage() {
+  return <UserScreen userInfo={{}} posts={{}} />;
 }
 
 export default websitePageHOC(OtherUsersProfilePage, {
@@ -17,34 +18,34 @@ export default websitePageHOC(OtherUsersProfilePage, {
   },
 });
 
-export async function getStaticProps({ params }) {
-  const { id } = params;
-  const dados = await userService().getProfilePage();
+// export async function getStaticProps({ params }) {
+//   const { id } = params;
+//   const dados = await userService.getProfilePage(null);
 
-  const posts = dados.posts.filter((post) => post.user === id);
-  const { userInfo } = dados;
+//   const posts = dados.posts.filter((post) => post.user === id);
+//   const { userInfo } = dados;
 
-  return {
-    props: {
-      userInfo,
-      posts,
-    },
-  };
-}
+//   return {
+//     props: {
+//       userInfo,
+//       posts,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const auth = authService(null);
+// export async function getStaticPaths() {
+//   // const auth = authService(null);
 
-  // fetch nessa api
-  // https://instalura-api-git-master-omariosouto.vercel.app/api/users
+//   // // fetch nessa api
+//   // // https://instalura-api-git-master-omariosouto.vercel.app/api/users
 
-  const user = await auth.getSession();
-  console.log('user', user);
+//   // const user = await auth.getSession();
+//   // console.log('user', user);
 
-  return {
-    paths: [{
-      params: { id: user.id },
-    }],
-    fallback: false,
-  };
-}
+//   return {
+//     paths: [{
+//       params: { id: '5fe9035f5bb019a3c62572da' },
+//     }],
+//     fallback: false,
+//   };
+// }
