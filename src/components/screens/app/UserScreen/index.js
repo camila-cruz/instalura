f1ad6acb58b00c62d32c91c2fcc6339fc7775307
Camila <cruz.camila001@gmail.com>
2021-05-01 11:15:29 -0300

    feat: cria grid para exibir posts do usuário no seu perfil

--- src/components/screens/app/UserScreen/index.js
+++ src/components/screens/app/UserScreen/index.js
@@ -8,7 +8,7 @@ import { Box } from '../../../foundation/layout/Box';
 function UserStats({ statsCount, statsTitle }) {
   return (
     <Grid.Col
-      value={{ xs: 3 }}
+      value={{ xs: 3, md: 1 }}
       display="flex"
       flexDirection="column"
       justifyContent="center"
@@ -34,19 +34,55 @@ UserStats.propTypes = {
   statsTitle: PropTypes.string.isRequired,
 };
 
+function UserPostsRow(posts) {
+  return (
+    <Grid.Row marginBottom={{ xs: '4px', md: '32px' }}>
+      {posts.map(() => (
+        <Grid.Col value={4} display="flex" justifyContent="center">
+          <img src="https://via.placeholder.com/95" alt="" />
+        </Grid.Col>
+      ))}
+    </Grid.Row>
+  );
+}
+
 function UserPosts() {
   const arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+  const gridRowLength = 3;
+
+  const postsDividedByChunks = arr.reduce((chunk, item, index) => {
+    const chunkIndex = Math.floor(index / gridRowLength);
+
+    if (!chunk[chunkIndex]) {
+      // eslint-disable-next-line no-param-reassign
+      chunk[chunkIndex] = []; // start a new chunk
+    }
+
+    chunk[chunkIndex].push(item);
+
+    return chunk;
+  }, []);
+
   return (
-    arr.map(() => <img src="https://via.placeholder.com/95" alt="" />)
+    <Grid.Col value={{ md: 8 }} offset={{ md: 2 }}>
+      {postsDividedByChunks.map((postsChunk) => UserPostsRow(postsChunk))}
+    </Grid.Col>
   );
 }
 
 export default function UserScreen() {
   return (
-    <Grid.Container>
-      <Grid.Row>
+    <Grid.Container
+      marginTop={{ xs: '24px', md: '64px' }}
+    >
+      <Grid.Row
+        justifyContent="center"
+        columnGap={{ md: '10px' }}
+      >
         <Grid.Col
-          value={{ xs: 3 }}
+          value={{ xs: 3, md: 1 }}
+          // offset={{ md: 3 }}
           // padding={0}
         >
           <Avatar
